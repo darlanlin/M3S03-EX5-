93c17d9d05ff4ffdbf9d5707a284d6019db0b68d
darlanlin <73296520+darlanlin@users.noreply.github.com>
2022-11-03 02:00:15 -0300

    Exercício 5 Módeulo 3 Semana 3

---
+++ src/main/java/br/com/futurodev/controllers/PessoaController.java
@@ -0,0 +1,45 @@
+package br.com.futurodev.controllers;
+
+import br.com.futurodev.model.PessoaModel;
+import br.com.futurodev.repository.PessoaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/pessoa")
+public class PessoaController {
+
+    @Autowired
+    private PessoaRepository pessoaRepository;
+
+    @RequestMapping(value = "/{nome}", method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
+    public String mostrarnome(@PathVariable String nome) {
+        return "O seu nome é " + nome;
+    }
+
+    @PostMapping(value = "/", produces = "application/json")
+    public ResponseEntity<PessoaModel> cadastrar(@RequestBody PessoaModel pessoa) {
+        PessoaModel modelo = pessoaRepository.save(pessoa);
+        return new ResponseEntity<PessoaModel>(modelo, HttpStatus.CREATED);
+    }
+
+    @PutMapping(value = "/", produces = "application/json")
+    public ResponseEntity<PessoaModel> atualizar(@RequestBody PessoaModel pessoa) {
+        PessoaModel modelo = pessoaRepository.save(pessoa);
+        return new ResponseEntity<PessoaModel>(modelo, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping(value = "/")
+    @ResponseBody
+    public ResponseEntity<String> delete (@RequestParam Long idUsuario) {
+        pessoaRepository.deleteById(idUsuario);
+        return new ResponseEntity<String>("Usuário deletado com sucesso!", HttpStatus.OK);
+
+    }
+
+
+
+}
---
+++ src/main/java/br/com/futurodev/repository/PessoaRepository.java
@@ -0,0 +1,9 @@
+package br.com.futurodev.repository;
+
+import br.com.futurodev.model.PessoaModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PessoaRepository extends JpaRepository<PessoaModel, Long> {
+}
